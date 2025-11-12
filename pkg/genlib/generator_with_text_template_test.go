a78c80742b0f9d29f6d5ecb9ab0f52a3678bcb7c
Domenico Andreoli <domenico.andreoli@elastic.co>
2025-11-12 10:28:53 +0100

    Add `long` type to the cardinality tests

--- pkg/genlib/generator_with_text_template_test.go
+++ pkg/genlib/generator_with_text_template_test.go
@@ -33,6 +33,7 @@ func Test_CardinalityWithTextTemplate(t *testing.T) {
 
 	test_CardinalityTWithTextTemplate[string](t, FieldTypeKeyword)
 	test_CardinalityTWithTextTemplate[int32](t, FieldTypeInteger)
+	test_CardinalityTWithTextTemplate[int64](t, FieldTypeLong)
 	test_CardinalityTWithTextTemplate[float64](t, FieldTypeFloat)
 	test_CardinalityTWithTextTemplate[string](t, FieldTypeGeoPoint)
 	test_CardinalityTWithTextTemplate[string](t, FieldTypeIP)
@@ -41,7 +42,7 @@ func Test_CardinalityWithTextTemplate(t *testing.T) {
 
 func test_CardinalityTWithTextTemplate[T any](t *testing.T, ty string) {
 	template := []byte(`{"alpha":"{{generate "alpha"}}", "beta":"{{generate "beta"}}"}`)
-	if ty == FieldTypeInteger || ty == FieldTypeFloat {
+	if ty == FieldTypeInteger || ty == FieldTypeLong || ty == FieldTypeFloat {
 		template = []byte(`{"alpha":{{generate "alpha"}}, "beta":{{generate "beta"}}}`)
 	}
 
